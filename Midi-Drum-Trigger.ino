d8c97c0adef2a7dc6401ef2d4a05edf44a752d04
Cruisen <GitHub.com@yoso.de>
2020-05-21 22:04:37 +0200

    added DEBUG

--- Midi-Drum-Trigger.ino
+++ Midi-Drum-Trigger.ino
@@ -13,7 +13,9 @@
 ////////////////
 
 // Arduino PADs
-int pad1 = A3;
+int padMax = 1;
+int pad[] = {A3};        // first element is pad[0] !!!
+byte midiKey[] = {38};   // The Midi Channel for this Pad
 
 // Arduino baudRate
 int baudRate = 115200;
@@ -24,11 +26,10 @@ byte midiChannel = 0 ; // 0-15 for 1 to 16
 byte noteOn = 144 + midiChannel; 
 byte noteOff = 128 + midiChannel;
 
-byte midiKey = 38;
-
 // Dead Times for AD sampling
-int deadTime1 = 50;
-int deadTime2 = 50;
+int deadTime1 = 50;    // between Midi Notes On and Midi Notes Off
+int deadTime2 = 50;    // between Midi Notes Off and Midi Notes On
+int deadTime3 = 0;     // between individual Midi Messages
 
 // Calibration AD to MIDI, with Low Cut
 float analogResolution = 1024.;	// Analog Digital Amplitude Resolution
@@ -40,9 +41,23 @@ float calibrationGradient = ( ( midiResolution - 1 ) / ( ( analogResolution - 1 
 // Variables for IN and OUT
 int midiVal; 
 
+
 ////////////////
 // FUNCTIONS
 ////////////////
+
+// Debug Function
+
+//#define DEBUG
+#ifdef DEBUG
+   #define DEBUG_PRINT(x)     Serial.print(x)
+   #define DEBUG_PRINTDEC(x)  Serial.print(x, DEC)
+   #define DEBUG_PRINTLN(x)   Serial.println(x)
+#else
+   #define DEBUG_PRINT(x)
+   #define DEBUG_PRINTDEC(x)
+   #define DEBUG_PRINTLN(x)
+#endif
 
 // Read form Analog pad
 int readAnalog( int padLocal )
@@ -60,20 +75,54 @@ int readAnalog( int padLocal )
 // Write a MIDI Message
 void MIDImessage( byte commandLocal, byte dataLocal1, byte dataLocal2 )
 {
-    Serial.write( commandLocal );
-    Serial.write( dataLocal1 );
-    Serial.write( dataLocal2 );
+    #ifdef DEBUG
+        DEBUG_PRINT("commandLocal: ");
+        DEBUG_PRINTDEC(commandLocal);
+        
+        DEBUG_PRINT(", dataLocal1: ");
+        DEBUG_PRINTDEC(dataLocal1);
+        
+        DEBUG_PRINT(", dataLocal2:");
+        DEBUG_PRINTDEC(dataLocal2);
+
+        DEBUG_PRINTLN("");
+
+    #else
+        Serial.write( commandLocal );
+        Serial.write( dataLocal1 );
+        Serial.write( dataLocal2 );
+    #endif
 } ;
 
-// send Midi Note On and than Off, with velocity
-void midiOnOff( byte velocityLocal, int deadTimeLocal1, int deadTimeLocal2 ) 
-{
-    MIDImessage( noteOn, midiKey, velocityLocal );   // turn note on
-    delay( deadTimeLocal1 );
 
-    MIDImessage( noteOff, midiKey, 0 );     // turn note off
-    delay( deadTimeLocal2 );  
+// Midi ON
+void midiOn()
+{
+    int i;
+    
+    for ( i = 0 ; i < padMax ; i++ ) {
+        // Get Analog reading converted to Midi velocity
+        midiVal = readAnalog( pad[i] );
+    
+        // disregard negative Values (due to Low Cut Filter calibrationOffSet = 120)
+        if ( midiVal > 0 )
+        {
+            MIDImessage( noteOn, midiKey[i], midiVal );   // turn note on
+            delay( deadTime3 );
+        }
+    }  
 }
+
+void midiOff()
+{
+    int i;
+    
+    for ( i = 0 ; i < padMax ; i++ ) {
+        MIDImessage( noteOff, midiKey[i], 0 );     // turn note off
+        delay( deadTime3 );
+    }
+}
+
 
 ////////////////
 // ARDUINO Stetup and Loop
@@ -82,19 +131,26 @@ void midiOnOff( byte velocityLocal, int deadTimeLocal1, int deadTimeLocal2 )
 // Arduino SETUP
 void setup()
 {
-    Serial.begin( baudRate );
-    pinMode( pad1, INPUT );
+    int i;
+
+    #ifdef DEBUG
+        Serial.begin(9600);
+    #else
+        Serial.begin( baudRate );
+    #endif
+    
+    // init ALL PADs as INPUT
+    for ( i = 0 ; i < padMax ; i++ ) {
+        pinMode( pad[i], INPUT );
+    }
 }
 
 // Arduino MAIN LOOP
 void loop()
 {
-   // Get Anlog reading converted to Midi velocity
-   midiVal = readAnalog( pad1 );
-
-   // disregard negative Values (due to Low Cut Filter calibrationOffSet = 120)
-   if ( midiVal > 0 )
-   {
-        midiOnOff( midiVal, deadTime1, deadTime2 );
-   }
+    midiOn();
+    delay( deadTime1 );
+    
+    midiOff();
+    delay( deadTime2 );
 }
